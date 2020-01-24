963816dad10b78ebd3f15d9db39e5be262835afa
Joell <amamihechi.udu@decagon.dev>
2020-01-24 14:24:02 +0100

    accessor methods and properties

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,28 @@
+﻿namespace ACM.BL {
+    public class Customer {
+        private string _lastName;
+        private string _firstName;
+        private string _email;
+
+        public string LastName {
+            get { return _lastName; }
+            set { _lastName = value; }
+            }
+
+        public string FirstName {
+            get { return _firstName; }
+            set { _firstName = value; }
+            }
+
+        public string Email {
+            get { return _email; }
+            set { _email = value; }
+            }
+
+        public string Phone { get; set; }
+        public int any { get; private set; }
+
+
+
+        }
+    }
