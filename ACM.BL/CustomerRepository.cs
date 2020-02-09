f9f789f6320039c4bed677541f6c9f957024287b
Joell <amamihechi.udu@decagon.dev>
2020-02-09 16:34:41 +0100

    cusomer repos

--- ACM.BL/CustomerRepository.cs
+++ ACM.BL/CustomerRepository.cs
@@ -2,5 +2,18 @@
 {
     public class CustomerRepository
     {
+        public Customer Retrieve(int customerId)
+        {
+            Customer customer = new Customer(customerId);
+
+
+            if (customerId == 1)
+            {
+                customer.Email = "Clarkkent@krypton.com";
+                customer.FirstName = "Super";
+                customer.LastName = "Man";
+            }
+            return customer;
+        }
     }
 }
