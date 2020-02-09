a3ebbda4960d57d6420901fd22a6a8a1db4bead5
Joell <amamihechi.udu@decagon.dev>
2020-02-09 14:58:21 +0100

    added product class

---
+++ ACM.BL/Product.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace ACM.BL
+{
+    class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            this.productId = productId;
+        }
+        public int productId { get; private set; }
+        public string ProductName { get; set; }
+        public string description { get; set; }
+        public float currentPrice { get; set; }
+
+
+        public bool Validate()
+        {
+            return true;
+        }
+
+        public Product Retrieve(int id)
+        {
+            return new Product();
+        }
+
+        public List<Product> Retrieve()
+        {
+            return new List<Product>();
+        }
+
+        public bool Save()
+        {
+            //save logic
+            return true;
+        }
+
+
+    }
+}
